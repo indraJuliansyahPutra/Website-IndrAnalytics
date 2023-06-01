8e722c8fd1b21509102cf9c28fd733488a797e89
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2023-06-02 06:56:48 +0700

    First Commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap core JavaScript -->
+    <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- Additional Scripts -->
+    <script src="<?= base_url('assets/'); ?>js/custom.js"></script>
+    <script src="<?= base_url('assets/'); ?>js/owl.js"></script>
+  </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="<?= base_url('assets/'); ?>images/untitled/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>IndrAnalytics.com | Website Portofolio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/fontawesome.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/owl.css">
+
+  </head>
+
+  <body>
